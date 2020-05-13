280ad2391c7247ffcb12720f164997a089449dbc
diamond <cuonggvu97@gmail.com>
2020-05-13 20:41:17 +0700

    Upload first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    {{--    Font--}}
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css"
+          integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-sm navbar-dark fixed-top" style="background-color: #ffffff;">
+    <a class="navbar-brand" href="#"><img src="./image/logo1.svg" alt="" width="120px" height="60px"></a>
+    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation"></button>
+    <div class="collapse navbar-collapse" id="collapsibleNavId">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <a href="#" class="nav-link">Login</a>
+            <a href="#" class="nav-link">Sign up</a>
+            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Get a quote</button>
+        </form>
+    </div>
+</nav>
+<div class="row">
+    <div class="col-lg-7">
+        <div class="row">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-11">
+                <h1>Your Brand on Your Links</h1>
+                <h3>Rebrandly is the industry-leading link management platform to brand, track and share short URLs
+                    using a custom domain name</h3>
+                <div class="btnHeader">
+                    <button type="button" class="btn btn-primary btn-lg" id="btnLeft">Sign up free</button>
+                    <button type="button" class="btn btn-outline-primary btn-lg">Request a demo</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-5">
+        <img src="./image/avata.png" alt="Avatar">
+    </div>
+</div>
+<div class="row">
+    <form class="form-inline" id="search">
+        <div class="form-group" id="textSearch">
+            {{--            <label for="inputPassword2" class="sr-only">Password</label>--}}
+            <input type="password" class="form-control" placeholder="Make your links shorter" size="60px">&nbsp;&nbsp;
+            <button type="submit" class="btn btn-primary">Shorter URL</button>
+        </div>
+    </form>
+</div>
+</div>
+
+<div class="row">
+    <div class="learn_more">
+        <div class="chill">
+            <span>Learn more</span>
+            <i class="fas fa-angle-down"></i>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="blue_banner">
+        <span>The complete platform to brand and manage links</span>
+        <div class="row">
+            <div class="col-lg-2">a</div>
+            <div class="col-lg-8">
+                <img src="./image/hero-home-2.png" alt="" width="100%">
+            </div>
+            <div class="col-lg-2">a</div>
+        </div>
+        <button class="btn btn-outline-primary btn-lg">Discover all features</button>
+    </div>
+</div>
+
+<div class="row">
+    <span id="new_h1_dark">Trusted by the world’s smartest brands</span>
+</div>
+
+<div class="row" style="margin-left: 10px">
+    <div class="col-lg-1"></div>
+    <div class="col-lg-2">
+        <img src="./image/ey.svg" alt="">
+    </div>
+    <div class="col-lg-2">
+        <img src="./image/harvard.svg" alt="">
+    </div>
+    <div class="col-lg-2">
+        <img src="./image/ibm.svg" alt="">
+    </div>
+    <div class="col-lg-2">
+        <img src="./image/three.svg" alt="">
+    </div>
+    <div class="col-lg-2">
+        <img src="./image/shopify.svg" alt="">
+    </div>
+    <div class="col-lg-1"></div>
+</div>
+
+<div class="row">
+    <span id="new_h2_dark">Rebrandly connects to the tools you already use</span>
+    <img src="./image/integrations-ill.svg" alt="" width="95%">
+</div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
